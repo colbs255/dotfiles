1978529a1c5e33eb4e1311547846a0b53a562750
colbychance <duncanchance11@gmail.com>
2022-11-29 23:56:04 -0500

    Simplify config

---
+++ nvim/.config/nvim/after/plugin/keymap.lua
@@ -0,0 +1,39 @@
+local Remap = require("colbs.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
+
+-- ================ General =====================
+nnoremap("<leader>-", ":Ex<CR>")
+-- Prevents delete char from overwriting register
+nnoremap("x", '"_x')
+-- Prevents visual paste from overwriting register
+vnoremap("p", '"_dP')
+
+-- ================ Telescope =====================
+nnoremap("<C-p>", function()
+    require('telescope.builtin').git_files()
+end)
+
+nnoremap("<Leader>ff", function()
+    require('telescope.builtin').find_files()
+end)
+
+nnoremap("<Leader>fg", function()
+    require('telescope.builtin').live_grep()
+end)
+
+nnoremap("<Leader>fb", function()
+    require('telescope.builtin').buffers()
+end)
+
+nnoremap("<Leader>fh", function()
+    require('telescope.builtin').help_tags()
+end)
+
+-- ================ Neogit =====================
+nnoremap("<Leader>gs", function()
+    require('neogit').open()
+end)
