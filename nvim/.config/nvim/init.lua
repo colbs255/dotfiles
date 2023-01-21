39791eb34f17666797ea6b326dbbbbf10d0962f1
colbychance <duncanchance11@gmail.com>
2023-01-21 15:58:13 -0500

    LAZY

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,3 @@
--- Speeds up start time via caching
-require("impatient")
-
 require("user.settings")
-require("user.plugins")
+require("user.plugin")
 require("user.keymaps")
-require("user.colorscheme")
-
-require("user.neogit")
-require("user.treesitter")
-require("user.lsp")
-require("user.lualine")
