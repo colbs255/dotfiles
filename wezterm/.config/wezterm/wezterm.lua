55d618b62f9495a859a65a898a1fa2d530fc099c
Colby Chance <duncanchance11@gmail.com>
2023-02-02 23:58:11 -0500

    Progress

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,12 @@ local wezterm = require 'wezterm'
 return {
     font = wezterm.font 'JetBrains Mono',
     font_size = 18.0,
-    color_scheme = 'Adventure'
+    color_scheme = "Catppuccin Mocha",
+    hide_tab_bar_if_only_one_tab = true,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+  },
 }
-
