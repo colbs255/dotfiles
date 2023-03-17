f92fa4c0cc67b6efaffcc9e3e600f0129f073a3d
Colby Chance <duncanchance11@gmail.com>
2023-03-16 21:20:01 -0400

    Cleanup remappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,3 @@
 require("user.settings")
 require("user.plugin")
-require("user.keymaps")
+require("user.reymaps")
--- nvim/.config/nvim/lua/user/reymaps.lua
+++ nvim/.config/nvim/lua/user/reymaps.lua
@@ -1,9 +1,21 @@
-local Remap = require("user.keymap")
-local nnoremap = Remap.nnoremap
-local vnoremap = Remap.vnoremap
-local inoremap = Remap.inoremap
-local xnoremap = Remap.xnoremap
-local nmap = Remap.nmap
+-- Makes vim mapping more convenient in lua configs
+local function bind(op, outer_opts)
+    outer_opts = outer_opts or {noremap = true}
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend("force",
+            outer_opts,
+            opts or {}
+        )
+        vim.keymap.set(op, lhs, rhs, opts)
+    end
+end
+
+local nmap = bind("n", {noremap = false})
+local nnoremap = bind("n")
+local vnoremap = bind("v")
+local xnoremap = bind("x")
+local inoremap = bind("i")
+
 -- TOOD: Use keymap?
 -- local keymap = vim.api.nvim_set_keymap
 
