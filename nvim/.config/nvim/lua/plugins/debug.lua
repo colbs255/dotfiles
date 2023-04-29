3a1e695174a766c5e939124f3d9dee7a877da344
Colby Chance <colby@Colbys-MacBook-Pro.local>
2023-04-29 15:11:21 -0400

    Add debug keys

--- nvim/.config/nvim/lua/plugins/debug.lua
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -5,5 +5,14 @@ return {
             "rcarriga/nvim-dap-ui",
             config = true
         }
-    }
+    },
+    keys = {
+        { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+        { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+        { "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+        { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+        { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+        { "<leader>dr", function() require("dap").repl.open() end, desc = "Repl" },
+    },
 }
