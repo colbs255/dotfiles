ebee140d3b648d5d90a12679e4dc84c569e72f6d
colbs255 <colby@dev.com>
2023-05-09 23:12:10 -0400

    zoxide

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -31,3 +31,5 @@ function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
 end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+zoxide init fish | source
