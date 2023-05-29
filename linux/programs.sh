aceae79e8630bae91978e18323cc08b475e03cb6
colbs255 <colby@dev.com>
2023-05-29 13:58:51 -0400

    Fix linux install path

--- linux/programs.sh
+++ linux/programs.sh
@@ -9,9 +9,9 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 # Lazygit repo
 sudo dnf -y copr enable atim/lazygit
 
-sudo dnf -y install $(cat linux/packages.txt)
+sudo dnf -y install $(cat packages.txt)
 
-sudo dnf -y remove $(cat linux/remove.txt)
+sudo dnf -y remove $(cat remove.txt)
 
 echo "Note: you may have to run 'sudo alternatives --config java' to choose your java"
 
