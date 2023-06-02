156f0f378a13e7edd444aae0baf5af14b9f0483b
colbs255 <colby@dev.com>
2023-06-01 20:56:17 -0400

    Lint

--- config/nvim/.config/nvim/lua/plugins/neogit.lua
+++ config/nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,20 +1,20 @@
 return {
-    "TimUntersberger/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- Won't load neogit until we call this mapping
-    keys = {
-        {
-            "<Leader>gg",
-            function()
-                require("neogit").open()
-            end,
-        },
-    },
-    config = function()
-        require("neogit").setup({
-            disable_commit_confirmation = true,
-        })
-    end,
+	"TimUntersberger/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	-- Won't load neogit until we call this mapping
+	keys = {
+		{
+			"<Leader>gg",
+			function()
+				require("neogit").open()
+			end,
+		},
+	},
+	config = function()
+		require("neogit").setup({
+			disable_commit_confirmation = true,
+		})
+	end,
 }
--- config/wezterm/.config/wezterm/wezterm.lua
+++ config/wezterm/.config/wezterm/wezterm.lua
@@ -1,30 +1,30 @@
 local wezterm = require("wezterm")
 
 local osToBinPath = {
-    linux = "/usr/bin",
-    darwin = "/opt/homebrew/bin"
+	linux = "/usr/bin",
+	darwin = "/opt/homebrew/bin",
 }
 
-local function loadShellPathForOS ()
-    local handle = io.popen("uname -s | tr '[:upper:]' '[:lower:]'")
-    local osName = string.gsub(handle:read("*a"), "\n", "")
-    handle:close()
-    return osToBinPath[osName]
+local function loadShellPathForOS()
+	local handle = io.popen("uname -s | tr '[:upper:]' '[:lower:]'")
+	local osName = string.gsub(handle:read("*a"), "\n", "")
+	handle:close()
+	return osToBinPath[osName]
 end
 
 local program_path = loadShellPathForOS() .. "/fish"
 
 return {
-    default_prog = { program_path, "-l" },
-    font = wezterm.font("JetBrains Mono"),
-    font_size = 18.0,
-    color_scheme = "Catppuccin Mocha",
-    hide_tab_bar_if_only_one_tab = true,
-    cursor_blink_rate = 800,
-    window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
-    },
+	default_prog = { program_path, "-l" },
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 18.0,
+	color_scheme = "Catppuccin Mocha",
+	hide_tab_bar_if_only_one_tab = true,
+	cursor_blink_rate = 800,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 }
