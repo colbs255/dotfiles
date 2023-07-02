1b1fad57025b45c836a998ce76710c9a7374ab4c
colbs255 <colby@dev.com>
2023-07-02 11:35:17 -0400

    XPLR - config indentation

--- config/xplr/.config/xplr/init.lua
+++ config/xplr/.config/xplr/init.lua
@@ -19,185 +19,185 @@ xplr.config.node_types.extension.adoc = { meta = { icon = " " } }
 -- ### Layouts ----------------------------------------------------------------
 -- ###########################################################################
 xplr.config.layouts.builtin.default = {
-  Horizontal = {
-    config = {
-      constraints = {
-        { Percentage = 70 },
-        { Percentage = 30 },
-      },
-    },
-    splits = {
-      {
-        Vertical = {
-          config = {
+    Horizontal = {
+        config = {
             constraints = {
-              { Min = 1 },
-              { Length = 3 },
-              { Length = 3 },
+                { Percentage = 70 },
+                { Percentage = 30 },
             },
-          },
-          splits = {
-            "Table",
-            "SortAndFilter",
-            "InputAndLogs",
-          },
         },
-      },
-      {
-        Vertical = {
-          config = {
-            constraints = {
-              { Percentage = 30 },
-              { Percentage = 70 },
+        splits = {
+            {
+                Vertical = {
+                    config = {
+                        constraints = {
+                            { Min = 1 },
+                            { Length = 3 },
+                            { Length = 3 },
+                        },
+                    },
+                    splits = {
+                        "Table",
+                        "SortAndFilter",
+                        "InputAndLogs",
+                    },
+                },
             },
-          },
-          splits = {
-            "Selection",
-            "HelpMenu",
-          },
+            {
+                Vertical = {
+                    config = {
+                        constraints = {
+                            { Percentage = 30 },
+                            { Percentage = 70 },
+                        },
+                    },
+                    splits = {
+                        "Selection",
+                        "HelpMenu",
+                    },
+                },
+            },
         },
-      },
     },
-  },
 }
 
 -- The layout without help menu
 xplr.config.layouts.builtin.no_help = {
-  Horizontal = {
+    Horizontal = {
     config = {
-      constraints = {
-        { Percentage = 70 },
-        { Percentage = 30 },
-      },
-    },
-    splits = {
-      {
-        Vertical = {
-          config = {
-            constraints = {
-              { Length = 3 },
-              { Min = 1 },
-              { Length = 3 },
+        constraints = {
+                { Percentage = 70 },
+                { Percentage = 30 },
             },
-          },
-          splits = {
-            "SortAndFilter",
-            "Table",
-            "InputAndLogs",
-          },
         },
-      },
-      "Selection",
+        splits = {
+            {
+                Vertical = {
+                    config = {
+                        constraints = {
+                            { Length = 3 },
+                            { Min = 1 },
+                            { Length = 3 },
+                        },
+                    },
+                    splits = {
+                        "SortAndFilter",
+                        "Table",
+                        "InputAndLogs",
+                    },
+                },
+            },
+            "Selection",
+        },
     },
-  },
 }
 
 -- The layout without selection panel
 xplr.config.layouts.builtin.no_selection = {
-  Horizontal = {
-    config = {
-      constraints = {
-        { Percentage = 70 },
-        { Percentage = 30 },
-      },
-    },
-    splits = {
-      {
-        Vertical = {
-          config = {
+    Horizontal = {
+        config = {
             constraints = {
-              { Length = 3 },
-              { Min = 1 },
-              { Length = 3 },
+                { Percentage = 70 },
+                { Percentage = 30 },
             },
-          },
-          splits = {
-            "SortAndFilter",
-            "Table",
-            "InputAndLogs",
-          },
         },
-      },
-      "HelpMenu",
+        splits = {
+            {
+                Vertical = {
+                    config = {
+                        constraints = {
+                            { Length = 3 },
+                            { Min = 1 },
+                            { Length = 3 },
+                        },
+                    },
+                    splits = {
+                        "SortAndFilter",
+                        "Table",
+                        "InputAndLogs",
+                    },
+                },
+            },
+            "HelpMenu",
+        },
     },
-  },
 }
 
 -- The layout without help menu and selection panel
 xplr.config.layouts.builtin.no_help_no_selection = {
-  Vertical = {
-    config = {
-      constraints = {
-        { Length = 3 },
-        { Min = 1 },
-        { Length = 3 },
-      },
+    Vertical = {
+        config = {
+            constraints = {
+                { Length = 3 },
+                { Min = 1 },
+                { Length = 3 },
+            },
+        },
+        splits = {
+            "SortAndFilter",
+            "Table",
+            "InputAndLogs",
+        },
     },
-    splits = {
-      "SortAndFilter",
-      "Table",
-      "InputAndLogs",
-    },
-  },
 }
 
 -- ###########################################################################
 -- ### File Table columns --------------------------------------------------
 -- ###########################################################################
 xplr.config.general.table.header.cols = {
-  { format = "",            style = {} },
-  { format = "╭─── Path", style = {} },
-  { format = "Size",              style = {} },
-  { format = "Modified",          style = {} },
-  { format = "",              style = {} },
+    { format = "",            style = {} },
+    { format = "╭─── Path", style = {} },
+    { format = "Size",              style = {} },
+    { format = "Modified",          style = {} },
+    { format = "",              style = {} },
 }
 
 xplr.config.general.table.col_widths = {
-  { Percentage = 10 },
-  { Percentage = 60 },
-  { Percentage = 10 },
-  { Percentage = 20 },
-  { Percentage = 0 },
+    { Percentage = 10 },
+    { Percentage = 60 },
+    { Percentage = 10 },
+    { Percentage = 20 },
+    { Percentage = 0 },
 }
 
 -- Index
 xplr.fn.builtin.fmt_general_table_row_cols_0 = function(m)
-  local r = ""
-  if m.is_before_focus then
-    r = r .. " -"
-  else
-    r = r .. "  "
-  end
+    local r = ""
+    if m.is_before_focus then
+        r = r .. " -"
+    else
+        r = r .. "  "
+    end
 
-  r = r .. m.relative_index .. "│" .. m.index
+    r = r .. m.relative_index .. "│" .. m.index
 
-  return r
+    return r
 end
 
 -- Size
 xplr.fn.builtin.fmt_general_table_row_cols_2 = function(m)
-  if not m.is_dir then
-    return m.human_size
-  else
-    return ""
-  end
+    if not m.is_dir then
+        return m.human_size
+    else
+        return ""
+    end
 end
 
 -- Time
 xplr.fn.builtin.fmt_general_table_row_cols_3 = function(m)
-  return tostring(os.date("%b %d, %H:%M:%S %Y", m.last_modified / 1000000000))
+    return tostring(os.date("%b %d, %H:%M:%S %Y", m.last_modified / 1000000000))
 end
 
 -- Perms
 xplr.fn.builtin.fmt_general_table_row_cols_4 = function(m)
-  return ""
+    return ""
 end
 
 -- ### Required --------------------------------------------------
 return {
-  on_load = {},
-  on_directory_change = {},
-  on_focus_change = {},
-  on_mode_switch = {},
-  on_layout_switch = {},
+    on_load = {},
+    on_directory_change = {},
+    on_focus_change = {},
+    on_mode_switch = {},
+    on_layout_switch = {},
 }
