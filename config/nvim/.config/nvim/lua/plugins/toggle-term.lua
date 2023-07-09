54da7cb21aad871f520be51dc91f011bb1f67b5a
colbs255 <colby@dev.com>
2023-07-08 23:44:45 -0400

    xplr

--- config/nvim/.config/nvim/lua/plugins/toggle-term.lua
+++ config/nvim/.config/nvim/lua/plugins/toggle-term.lua
@@ -1,8 +1,30 @@
-local function lazygit ()
+local function lazygit()
     local Terminal  = require('toggleterm.terminal').Terminal
     local lazygit = Terminal:new({
         cmd = "lazygit",
         dir = "git_dir",
+        direction = "float",
+        float_opts = {
+            border = "double",
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+            vim.cmd("startinsert!")
+            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+            vim.cmd("startinsert!")
+        end,
+    })
+    lazygit:toggle()
+end
+
+local function xplr()
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({
+        cmd = "xplr",
+        dir = vim.fn.expand('%:p:h'),
         direction = "float",
         float_opts = {
             border = "double",
@@ -27,6 +49,9 @@ return {
     keys = {
         {
             "<Leader>gg", lazygit
+        },
+        {
+            "<Leader>-", xplr
         },
     }
 }
