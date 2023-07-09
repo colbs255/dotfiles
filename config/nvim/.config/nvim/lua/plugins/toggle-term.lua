5fbb63786ef08d4526ed9e858976d717ceca8b0e
colbs255 <colbs255@users.noreply.github.com>
2023-07-09 09:39:11 -0400

    Toggle term (#48)
    Co-authored-by: colbs255 <colby@dev.com>

---
+++ config/nvim/.config/nvim/lua/plugins/toggle-term.lua
@@ -0,0 +1,32 @@
+local function lazygit()
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({
+        cmd = "lazygit",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+            border = "double",
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+            vim.cmd("startinsert!")
+            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+            vim.cmd("startinsert!")
+        end,
+    })
+    lazygit:toggle()
+end
+
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    keys = {
+        {
+            "<Leader>gg", lazygit
+        }
+    }
+}
