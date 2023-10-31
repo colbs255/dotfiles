1fe3da6d6b4132604a0b6c534628a701f9c46322
colbs255 <colbs255@users.noreply.github.com>
2023-10-30 22:25:25 -0400

    Vim indent guides (#57)
    Co-authored-by: colbs255 <colby@dev.com>

---
+++ config/nvim/.config/nvim/lua/plugins/indent-guide.lua
@@ -0,0 +1,22 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = {
+            char = "│",
+            tab_char = "│",
+        },
+        scope = { enabled = false },
+        exclude = {
+            filetypes = {
+                "help",
+                "alpha",
+                "dashboard",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+            },
+        },
+    },
+}
