22254ff3071b15f1f848ffc3835f1aa489bb723a
colbs255 <colbs255@users.noreply.github.com>
2023-11-27 20:19:51 -0500

    Nvim - transparant without numbers (#60)
    Co-authored-by: colbs255 <colby@dev.com>

--- config/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,9 +3,9 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-        -- load the colorscheme here
-        vim.g.tokyonight_transparent_sidebar = true
-        vim.g.tokyonight_transparent = true
+        require("tokyonight").setup({
+            transparent = true
+        })
         vim.opt.background = "dark"
         vim.cmd([[colorscheme tokyonight]])
     end,
