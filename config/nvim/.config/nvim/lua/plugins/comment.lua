c6e13339d7b86c63b564b5c81df787410b16aff2
colbs255 <colby@dev.com>
2023-12-02 01:01:31 -0500

    Nvim - mini.surround and mini.comment

--- config/nvim/.config/nvim/lua/plugins/comment.lua
+++ config/nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,5 @@
 return {
-    "numToStr/Comment.nvim",
+    "echasnovski/mini.comment",
     event = "VeryLazy",
     config = true,
 }
--- config/nvim/.config/nvim/lua/plugins/surround.lua
+++ config/nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,5 +1,15 @@
 return {
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
-    config = true,
+    "echasnovski/mini.surround",
+    opts = {
+        -- Change default bindings so they don't conflict with flash.nvim
+        mappings = {
+            add = "gza", -- Add surrounding in Normal and Visual modes
+            delete = "gzd", -- Delete surrounding
+            find = "gzf", -- Find surrounding (to the right)
+            find_left = "gzF", -- Find surrounding (to the left)
+            highlight = "gzh", -- Highlight surrounding
+            replace = "gzr", -- Replace surrounding
+            update_n_lines = "gzn", -- Update `n_lines`
+        },
+    },
 }
