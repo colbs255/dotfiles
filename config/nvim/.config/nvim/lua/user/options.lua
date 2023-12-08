8c5f0ee950a1b7f308be98e53a83a7a8005f5a80
colbs255 <colby@dev.com>
2023-12-07 21:25:24 -0500

    Nvim - save less revovery state (shaDa)

--- config/nvim/.config/nvim/lua/user/options.lua
+++ config/nvim/.config/nvim/lua/user/options.lua
@@ -21,6 +21,8 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+-- Vim state recovery, less state means faster startup
+vim.opt.shada = "!,'0,/0,:10,<0,@0,f0,h,s20"
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
