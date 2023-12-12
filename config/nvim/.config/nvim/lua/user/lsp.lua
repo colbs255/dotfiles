402fe67772fa794e1b02ea8298bd8ad2a131de33
colbs255 <colbs255@users.noreply.github.com>
2023-12-11 22:32:22 -0500

    Lsp (#61)
    Co-authored-by: colbs255 <colby@dev.com>

---
+++ config/nvim/.config/nvim/lua/user/lsp.lua
@@ -0,0 +1,33 @@
+local lsp_augroup = vim.api.nvim_create_augroup("user_lsp", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "haskell",
+    group = lsp_augroup,
+    callback = function()
+        local client = vim.lsp.start({
+            cmd = { 'haskell-language-server-wrapper', '--lsp' },
+            filetypes = { 'haskell', 'lhaskell' },
+            root_dir = vim.fs.dirname(vim.fs.find({'hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml'}, { upward = true })[1]),
+            single_file_support = true,
+        })
+        vim.lsp.buf_attach_client(0, client)
+    end,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = lsp_augroup,
+    desc = "LSP actions",
+    callback = function(event)
+        local opts = {buffer = event.buf}
+
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>cs", vim.lsp.buf.workspace_symbol, opts)
+    end
+})
