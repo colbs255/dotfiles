253ecff002e2adaee38544b4a94963fc87802154
colbs255 <colby@dev.com>
2023-12-11 23:10:15 -0500

    Nvim - load flash and minisurround on key mappings

--- config/nvim/.config/nvim/lua/plugins/flash.lua
+++ config/nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,6 +1,5 @@
 return {
     "folke/flash.nvim",
-    event = "VeryLazy",
     keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
