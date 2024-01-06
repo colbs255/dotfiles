83e3959aa8032f90d42ca4a8343ff74de1d90b12
colbs255 <colby@dev.com>
2024-01-06 13:17:48 -0500

    update readme

--- linux/install.sh
+++ linux/install.sh
@@ -5,7 +5,7 @@ nix-shell -p git home-manager
 # Clone repo
 git clone git@github.com:colbs255/dotfiles.git ~/dotfiles && cd ~/dotfiles
 # Setup home-manager
-home-manager -f ~/dotfiles/config/home.nix switch
+home-manager --extra-experimental-features nix-command --extra-experimental-features flakes switch --flake ~/dotfiles/config
 # Flatpaks
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak -y install flathub $(cat linux/flatpaks.txt)
