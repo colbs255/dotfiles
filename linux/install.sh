e87ca3b7cc5893656b9a1d0aeb2668e95eae9e7c
colbs255 <colby@dev.com>
2024-01-06 23:12:31 -0500

    Linux install

--- linux/install.sh
+++ linux/install.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+# Install nix
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+if [ -e /home/colby/.nix-profile/etc/profile.d/nix.sh ]; then . /home/colby/.nix-profile/etc/profile.d/nix.sh; fi
+
 # Setup shell with our dependencies
-nix-shell -p git home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell -p git home-manager gh
+ssh-keygen -t ed25519 -C "colby@dev.com"
+gh auth login
+
 # Clone repo
 git clone git@github.com:colbs255/dotfiles.git ~/dotfiles && cd ~/dotfiles
 # Setup home-manager
