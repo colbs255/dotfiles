28a7920ec5a15f531b0ccfbd4e239c2f0156a8b5
colbs255 <colby@dev.com>
2024-01-07 01:28:25 -0500

    save

--- linux/install.sh
+++ linux/install.sh
@@ -9,14 +9,14 @@ if [ -e /home/colby/.nix-profile/etc/profile.d/nix.sh ]; then . /home/colby/.nix
 # Setup shell with our dependencies
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
-nix-shell -p git home-manager gh
-ssh-keygen -t ed25519 -C "colby@dev.com"
-gh auth login
 
-# Clone repo
+# Clone git repo and install
+ssh-keygen -t ed25519 -C "colby@dev.com"
+nix-shell -p git home-manager gh --run '
+gh auth login
 git clone git@github.com:colbs255/dotfiles.git ~/dotfiles && cd ~/dotfiles
-# Setup home-manager
 home-manager --extra-experimental-features nix-command --extra-experimental-features flakes switch --flake ~/dotfiles/config
+'
 # Flatpaks
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak -y install flathub "$(cat linux/flatpaks.txt)"
