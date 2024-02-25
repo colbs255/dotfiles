5f66c317ea902ae5518bdae5fff1cd6721edf9dd
colbs255 <colby@dev.com>
2024-02-24 19:57:20 -0500

    Remove flatpaks

--- linux/install.sh
+++ linux/install.sh
@@ -17,9 +17,6 @@ gh auth login
 git clone git@github.com:colbs255/dotfiles.git ~/dotfiles && cd ~/dotfiles
 home-manager --extra-experimental-features nix-command --extra-experimental-features flakes switch --flake ~/dotfiles/config
 '
-# Flatpaks
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak -y install flathub "$(cat linux/flatpaks.txt)"
 
 # Settings
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
