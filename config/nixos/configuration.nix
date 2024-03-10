78a93e4da84464bdc336cf577d0d93b8ee05e353
colbs255 <colby@dev.com>
2024-03-09 19:03:18 -0500

    enable flakes

--- config/nixos/configuration.nix
+++ config/nixos/configuration.nix
@@ -58,6 +58,7 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   environment.systemPackages = with pkgs; [
     vim
     wget
---
+++ config/nixos/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "My nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
