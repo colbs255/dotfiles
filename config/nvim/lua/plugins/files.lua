901fadf1ef02326417509e667fcfbca06edddb76
colbs255 <colby@dev.com>
2024-04-06 00:02:13 -0400

    Add cd and window split keymaps to mini files

--- config/nvim/lua/plugins/files.lua
+++ config/nvim/lua/plugins/files.lua
@@ -1,3 +1,27 @@
+local set_split_mapping = function(buf_id, lhs, direction)
+    local rhs = function()
+        -- Make new window and set it as target
+        local new_target_window
+        vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+            vim.cmd(direction .. " split")
+            new_target_window = vim.api.nvim_get_current_win()
+        end)
+
+        MiniFiles.set_target_window(new_target_window)
+    end
+
+    -- Adding `desc` will result into `show_help` entries
+    local desc = "Split " .. direction
+    vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+local mini_files_set_cwd = function(path)
+    -- Works only if cursor is on the valid file system entry
+    local cur_entry_path = MiniFiles.get_fs_entry().path
+    local cur_directory = vim.fs.dirname(cur_entry_path)
+    vim.fn.chdir(cur_directory)
+end
+
 return {
     "echasnovski/mini.files",
     keys = {
@@ -16,5 +40,21 @@ return {
             desc = "Open file explorer (cwd)",
         },
     },
-    config = true,
+    config = function(opts)
+        require("mini.files").setup()
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniFilesBufferCreate",
+            callback = function(args)
+                local buf_id = args.data.buf_id
+                set_split_mapping(buf_id, "gs", "belowright horizontal")
+                set_split_mapping(buf_id, "gv", "belowright vertical")
+            end,
+        })
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniFilesBufferCreate",
+            callback = function(args)
+                vim.keymap.set("n", "g~", mini_files_set_cwd, { buffer = args.data.buf_id })
+            end,
+        })
+    end,
 }
