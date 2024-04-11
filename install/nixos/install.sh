abf37b4c4b450e5babf3547bc65f4d1c3d03602f
colbs255 <colby@dev.com>
2024-04-10 21:40:43 -0400

    save

--- install/nixos/install.sh
+++ install/nixos/install.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-# Setup shell with our dependencies
+echo "Installing homemanager channel"
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
-# Create ssk-key
+echo "Creating ssh key"
 ssh-keygen -t ed25519 -C "colby@dev.com"
 
-# Clone and install
-nix shell nixpkgs#git nixpkgs#home-manager nixpkgs#gh nixpkgs#bash --command bash -c '
+echo "Cloning and installing repo"
+nix shell nixpkgs#git nixpkgs#home-manager nixpkgs#gh nixpkgs#bash nixpkgs#just --command bash -c '
 gh auth login
 git clone git@github.com:colbs255/dotfiles.git ~/dotfiles && cd ~/dotfiles
-home-manager --extra-experimental-features nix-command --extra-experimental-features flakes switch --flake ~/dotfiles/config
+just build-home
+just build-system
 '
