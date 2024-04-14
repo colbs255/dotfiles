9133af8edcf7cf5cba4b381284d1f08bb4cd12b9
colbs255 <colby@dev.com>
2024-04-14 13:37:59 -0400

    fix install scripts

--- install/macos/install.sh
+++ install/macos/install.sh
@@ -13,8 +13,10 @@ echo "Creating ssh key"
 ssh-keygen -t ed25519 -C "colby@dev.com"
 
 echo "Cloning and installing repo"
-nix shell nixpkgs#git nixpkgs#home-manager nixpkgs#gh nixpkgs#bash nixpkgs#just --command bash -c '
+nix shell nixpkgs#git nixpkgs#gh nixpkgs#bash --command bash -c '
 gh auth login
-git clone git@github.com:colbs255/dotfiles.git ~/dotfiles && cd ~/dotfiles
-just build-home
+git clone git@github.com:colbs255/dotfiles.git ~/dotfiles
 '
+
+cd ~/dotfiles
+nix develop --command bash -c 'just build-home'
