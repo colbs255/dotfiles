530fedc67bc3ad36ddf6d5c4dc1369e139132aee
colbs255 <colby@dev.com>
2024-04-20 16:49:22 -0400

    looking good

--- config/fish/conf.d/fzf.fish
+++ config/fish/conf.d/fzf.fish
@@ -3,8 +3,8 @@ set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -gx FZF_ALT_C_COMMAND 'fd --type d --strip-cwd-prefix'
 
 set -gx FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:-1,spinner:#f5e0dc,hl:#32a852 \
+--color=bg+:#313244,bg:-1,spinner:#f5e0dc,hl:#b2e7ad \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#87ff00"
 
 fzf --fish | source
