f191ffc65fe68f33e41611e8548a2fc18739dcea
colbs255 <colby@dev.com>
2024-04-20 01:00:22 -0400

    FZF: use fd on alt-c

--- config/fish/conf.d/fzf.fish
+++ config/fish/conf.d/fzf.fish
@@ -1,3 +1,4 @@
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -gx FZF_ALT_C_COMMAND 'fd --type d --strip-cwd-prefix'
 fzf --fish | source
