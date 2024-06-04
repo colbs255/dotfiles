ebec0e8b5480221d660d1761e1c2ec3fbdfae2fa
colbs255 <colby@dev.com>
2024-06-03 22:36:47 -0400

    worksworks

---
+++ config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,8 @@
+return {
+    'echasnovski/mini.notify',
+    config = function()
+        local notify = require('mini.notify')
+        notify.setup()
+        vim.notify = notify.make_notify()
+    end,
+}
