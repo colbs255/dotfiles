0b8e665d13de7e06bad54df9c41665b1721f500d
colbs255 <colbs255@users.noreply.github.com>
2024-06-29 13:34:57 -0400

    Fish prompt (#117)
    Co-authored-by: colbs255 <colby@dev.com>

--- config/fish/conf.d/01-env.fish
+++ config/fish/conf.d/01-env.fish
@@ -4,18 +4,3 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 if status is-interactive
     fish_vi_key_bindings
 end
-
-function fish_prompt
-    set_color green
-    prompt_pwd --dir-length=0
-    echo '➜ '
-end
-
-function fish_greeting
-    set_color blue
-    echo Welcome $USER!
-end
-
-function fish_command_not_found
-    echo Command not found: (set_color red)$argv[1]
-end
