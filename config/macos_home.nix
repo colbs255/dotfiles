9344501ed0456d2486ef872d107926f1c92a6a49
colbs255 <colby@dev.com>
2024-07-10 10:54:43 +0200

    macos

---
+++ config/macos_home.nix
@@ -0,0 +1,99 @@
+{ pkgs, ... }:
+
+{
+  home.username = "colby";
+  home.homeDirectory = "/Users/colby";
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  xdg.configFile = {
+    lazygit.source = ./lazygit;
+    gitui.source = ./gitui;
+    ideavim.source = ./ideavim;
+    nvim.source = ./nvim;
+    fuzzel.source = ./fuzzel;
+    hypr.source = ./hypr;
+    waybar.source = ./waybar;
+    wallpapers.source = ./wallpapers;
+    fish.source = ./fish;
+    bat.source = ./bat;
+    alacritty.source = ./alacritty;
+    foot.source = ./foot;
+    zathura.source = ./zathura;
+    git.source = ./git;
+    tmux.source = ./tmux;
+  };
+  home.file.".bash_profile" = {
+    source = ./bash/.bash_profile;
+  };
+
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+  programs.home-manager.enable = true;
+  fonts.fontconfig.enable = true;
+
+  # gtk = {
+  #   enable = true;
+  #   theme = {
+  #     name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+  #     package = pkgs.catppuccin-gtk.override {
+  #       accents = [ "pink" ];
+  #       size = "compact";
+  #       tweaks = [
+  #         "rimless"
+  #         "black"
+  #       ];
+  #       variant = "macchiato";
+  #     };
+  #   };
+  # };
+
+  home.packages = with pkgs; [
+    neovim
+    # swaybg
+    # hyprlock
+    # waybar
+    git
+    gnumake
+    just
+    gcc
+    gh
+    ghc
+    cabal-install
+    haskell-language-server
+    lazygit
+    gitui
+    fish
+    alacritty
+    fzf
+    delta
+    asciidoctor
+    ripgrep
+    stow
+    tmux
+    fd
+    tree
+    wget
+    zoxide
+    yazi
+    bat
+    xdg-utils
+    bottom
+    sd
+    zathura
+    mupdf
+    eza
+    temurin-bin-21
+    shellcheck
+    nodePackages.bash-language-server
+    mpv
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    jetbrains.idea-community
+    inkscape
+  ];
+}
