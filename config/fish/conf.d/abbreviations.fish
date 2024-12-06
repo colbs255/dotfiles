1fddd0a8fbac04d51c41070d0c09afab613fda34
colbs255 <colby@dev.com>
2024-12-06 18:37:40 -0500

    Add gcd command

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -2,3 +2,4 @@ abbr --add lg lazygit
 abbr --add vi nvim
 abbr --add c 'bat --style=plain --paging=never'
 abbr --add ls 'exa -F --icons --group-directories-first'
+abbr --add gcd 'cd $(git rev-parse --show-toplevel)'
