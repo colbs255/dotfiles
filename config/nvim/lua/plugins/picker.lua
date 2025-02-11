01b2a8c2401b2d4bc5cac267d6a27dd4f4870096
colbs255 <colby@dev.com>
2025-02-10 22:18:38 -0500

    save

--- config/nvim/lua/plugins/picker.lua
+++ config/nvim/lua/plugins/picker.lua
@@ -1,24 +1,21 @@
-{
+return {
     "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-        picker = {
-            -- your picker configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    }
     keys = {
         -- stylua: ignore start
-        { "<Leader><space>", function() require("telescope.builtin").git_files() end, desc = "Find git files" },
-        { "<Leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files (root dir)" },
-        { "<Leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Help pages" },
-        { "<Leader>fm", function() require("telescope.builtin").man_pages() end, desc = "Man pages" },
-        { "<Leader>gb", function() require("telescope.builtin").git_branches() end, desc = "Git branches" },
-        { "<Leader>/", function() require("telescope.builtin").live_grep() end, desc = "Grep (root dir)" },
-        { "<Leader>,", function() require("telescope.builtin").buffers({ previewer = false }) end, desc = "Buffers" },
-        { "<Leader>:", function() require("telescope.builtin").command_history() end, desc = "Command History" },
-        { "z=", function() require("telescope.builtin").spell_suggest() end, desc = "Spell suggestions" },
+        { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+        { "<Leader>ff", function() Snacks.picker.files() end, desc = "Find files" },
+        { "<Leader>fh", function() Snacks.picker.help() end, desc = "Help pages" },
+        { "<Leader>/", function() Snacks.picker.grep() end, desc = "Grep (root dir)" },
+        { "<Leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+        { "<Leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+        { "z=", function() Snacks.picker.spelling() end, desc = "Spell suggestions" },
         -- stylua: ignore end
-    }
+    },
+    opts = {
+        picker = {
+            formatters = {
+                file = { filename_first = true }
+            },
+        }
+    },
 }
