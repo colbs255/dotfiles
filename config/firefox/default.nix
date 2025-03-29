d78f58d985c9c37f397d552c0fbcf6ebd530b550
colbs255 <colby@dev.com>
2025-03-28 22:07:40 -0400

    save

--- config/firefox/default.nix
+++ config/firefox/default.nix
@@ -72,22 +72,22 @@
       bookmarks = {
         force = true;
         settings = [
-        {
-          name = "Charles Schwab";
-          url = "https://www.schwab.com/";
-        }
-        {
-          name = "Router Settings";
-          url = "https://192.168.1.1/";
-        }
-        {
-          name = "Dotfiles";
-          url = "https://github.com/colbs255/dotfiles/";
-        }
-        {
-          name = "Github";
-          url = "https://github.com/";
-        }
+          {
+            name = "Charles Schwab";
+            url = "https://www.schwab.com/";
+          }
+          {
+            name = "Router Settings";
+            url = "https://192.168.1.1/";
+          }
+          {
+            name = "Dotfiles";
+            url = "https://github.com/colbs255/dotfiles/";
+          }
+          {
+            name = "Github";
+            url = "https://github.com/";
+          }
         ];
       };
 
