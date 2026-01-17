b372b86fc1fb20d00aec5251eb6c2b260cd1ef34
colbs255 <colbs255@users.noreply.github.com>
2026-01-17 17:14:06 -0500

    Update jan172025 (#140)
    Lazygit has breaking changes
    Fish has breaking changes
    Jetbrains no longer has idea-community. just normal idea

--- config/fish/conf.d/01-env.fish
+++ config/fish/conf.d/01-env.fish
@@ -3,5 +3,5 @@ set -gx XDG_CONFIG_HOME $HOME/.config
 fish_add_path -g ~/.local/bin
 
 if status is-interactive
-    fish_vi_key_bindings
+    set --global fish_key_bindings fish_vi_key_bindings
 end
